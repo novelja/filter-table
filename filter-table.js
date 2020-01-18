8eba647e284f9d4141ee98fdbf16cb77685d7a22
novelja <36472952+novelja@users.noreply.github.com>
2020-01-18 19:03:37 +0100

    Add files via upload

---
+++ filter-table.js
@@ -0,0 +1,46 @@
+(function ($) {
+
+  $.fn.filterTable = function ( id ) {
+
+    if (!id) {
+      console.error('You must provide filter input ID in options!');
+      return null;
+    }
+
+    // var settings = $.extend({
+    //   // These are the defaults.
+    //   id: ''
+    // }, options );
+
+    var filterID = id;
+    var table = $(this);
+    
+    $(filterID).on("keyup", function() {
+      var value = $(this).val().toUpperCase();
+
+      table.find('tr').each(function(index) {
+
+        $row = $(this);
+        let bool = false;
+
+        $row.find("td").each(function() {
+          $cell = $(this).text().toUpperCase();
+          if ($cell.indexOf(value) > -1) {  
+              bool = true;
+          }
+        });
+
+        if (bool) {  
+          $row.show();
+        }
+        else {
+          $row.hide();
+        }
+      }); // table each
+
+    }); // search keyup
+
+    return this;
+  }
+
+})(jQuery)
